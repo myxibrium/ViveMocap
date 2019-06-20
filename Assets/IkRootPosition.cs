f9f2612186c11bd3e634e2a260ab71a5f4ee904d
darriasc <darrias@repnox.com>
2019-06-20 13:35:14 -0400

    trying the VRIK rig... not liking it

---
+++ Assets/IkRootPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkRootPosition : MonoBehaviour
+{
+
+    public Transform headIkControl;
+    
+    public Transform bodyIkControl;
+
+    public Transform floorReference;
+
+    private float initialBodyIkRotation;
+
+    private void Start()
+    {
+        initialBodyIkRotation = bodyIkControl.rotation.y;
+    }
+
+    void LateUpdate()
+    {
+        var ave = (headIkControl.position + bodyIkControl.position) / 2;
+        transform.position = new Vector3(ave.x, floorReference?floorReference.position.y:0, ave.z);
+        var yrot = bodyIkControl.rotation.y - initialBodyIkRotation;
+        transform.rotation = Quaternion.Euler(0, yrot, 0);
+    }
+}
